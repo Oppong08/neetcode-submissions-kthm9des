dd3b7ecb850b7b7f75d621d34e98c0bbbcdf8acd
Nana Oppong Ampofo <ampofonanaoppong@gmail.com>
2026-04-04 11:44:57 -0500

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.py
@@ -0,0 +1,75 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+        def dfs(i,j):
+        #base case1: if we reach the end of word1, all remaning characters in word 2 must be inserted(insert n-j)
+            if i == m:
+                return n - j
+        #base case2: if we reach the end of word2, all remaining characters in word 1 must be deleted
+            if j == n:
+                return m - i
+            
+            if word1[i] == word2[j]:
+                return dfs(i + 1, j + 1)
+            
+            # res = min(dfs(i+1, j), dfs(i,j+1))
+            # res = min(res, dfs(i+1,j+1))
+            # return res + 1
+            insert = dfs(i, j + 1)
+            remove = dfs(i+1, j)
+            replace = dfs(i + 1, j+ 1)
+            return 1 + min(insert, remove, replace)
+            # return res + 1
+        return dfs(0,0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     cache = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+        
+    #     for j in range(len(word2) + 1):
+    #         cache[len(word1)][j] = len(word2) - j
+    #     for i in range(len(word1) + 1):
+    #         cache[i][len(word2)] = len(word1) - i
+
+    #     for i in range(len(word1) - 1, -1, -1):
+    #         for j in range(len(word2)-1, -1, -1):
+    #             if word1[i] == word2[j]:
+    #                 cache[i][j] = cache[i+ 1][j+1]
+                
+    #             else:                    #delete          #insert           #replace
+    #                 cache[i][j] = 1 + min(cache[i+1][j], cache[i][j+1], cache[i+1][j+1])
+
+    #     return cache[0][0]
