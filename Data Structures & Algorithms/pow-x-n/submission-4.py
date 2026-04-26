131d58a40f8b91daaa272050587a31fcd9424d93
Nana Oppong Ampofo <ampofonanaoppong@gmail.com>
2026-04-26 17:09:56 -0500

    Add: pow-x-n - submission-4

---
+++ Data Structures & Algorithms/pow-x-n/submission-4.py
@@ -0,0 +1,79 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def square(x,n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            res = square(x*x, n//2)
+            return x * res if n%2 else res
+
+        res = square(x, abs(n))
+        return res if n >= 0 else 1/res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #recursive
+        # def helper(x,n):
+        #     if x == 0:
+        #         return 0
+        #     if n == 0:
+        #         return 1
+            
+        #     res = helper(x * x, n//2)
+        #     # res = res * res
+            
+        #     return x * res if n%2 else res
+
+            
+
+        
+        # res = helper(x,abs(n))
+        # return res if n>= 0 else 1/res
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # return x ** n
+
