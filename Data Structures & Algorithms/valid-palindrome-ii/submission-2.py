dcd5036f965a79c103c419fb1ce6caa041f9ef4c
Nana Oppong Ampofo <ampofonanaoppong@gmail.com>
2026-05-20 08:40:12 -0700

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        #optimal two pointers
+        l,r = 0, len(s) - 1
+
+        while l<r:
+            if s[l] != s[r]:
+                skipL, skipR = s[l + 1 :], s[l :r]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+
+            l, r = l+1 , r-1
+
+        return True
+
+
+
+
+
+
+
+        # def isPalindrome(word):
+        #     return word == word[::-1]
+        
+        # for i in range(len(s)):
+        #     if isPalindrome(s[:i] + s[i+1:]):
+        #         return True
+        # return False
+    
+    
+
+        
