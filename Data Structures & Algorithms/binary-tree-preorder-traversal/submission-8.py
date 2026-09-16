af2555244ed53278b1c08c7f9e98353b7e3fda17
Nana Oppong Ampofo <ampofonanaoppong@gmail.com>
2026-09-16 01:08:59 -0500

    Add: binary-tree-preorder-traversal - submission-8

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-8.py
@@ -0,0 +1,111 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # #iterative dfs (simulate under the hood recursive stack)
+        res = []
+        stack= []
+        cur = root
+        while cur or stack:
+            if cur:
+                res.append(cur.val)
+                #push right first so that left can be processed first 
+                stack.append(cur.right)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+        return res
+        # stack = []
+        # res = []
+        # cur = root
+        # while stack or cur:
+        #     while cur:
+        #         res.append(cur.val)
+        #         stack.append(cur)
+        #         #go as far left
+        #         cur = cur.left
+        #     cur = stack.pop()
+        #     cur = cur.right
+        # return res
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = []
+        # stack = [] #stores the order of processing nodes
+        # cur = root
+
+        # while cur or stack:
+        #     #searches the left subtree unti it's null
+        #     while cur:
+        #         stack.append(cur)
+        #         res.append(cur.val)
+        #         cur = cur.left
+        #     cur = stack.pop()
+        #     cur = cur.right
+        # return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = []
+        # def bfs(root):
+        #     q = deque()
+        #     q.append(root)
+        #     while q:
+        #         for i in range(len(q)):
+        #             node = q.pop()
+        #             if node.left:
+        #                 q.append(node.left)
+        #             elif node.right:
+        #                 q.append(node.right)
+            
+        #             res.append(node.val)
+        # bfs(root)
+        # return res
+
+
+
+
+        # res = []
+        # def dfs(node):
+        #     if not node:
+        #         return
+        #     res.append(node.val)
+        #     dfs(node.left)
+        #     dfs(node.right)
+        # dfs(root)
+        # return res
